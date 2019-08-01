ba68670e9e90f1f37e3f7af17899f06d89e7dd7f
Simon Martyr <simon.martyr@finaps.nl>
2019-08-01 09:40:52 +0200

    first

---
+++ CSharpToGraphQLConverter.rb
@@ -0,0 +1,30 @@
+def GraphQLType(csharpType)
+  case csharpType.upcase
+  when "DECIMAL"
+    return "Float"
+  when "INT"
+    return "Int"
+  when "STRING"
+    return "String"
+  when "BOOLEAN"
+    return "Bool"
+  when "DATETIME"
+    return "String"
+  when "GUID"
+    return "ID"
+  else
+    "?"
+  end
+end
+
+File.open("sample.txt", "r") do |f|
+  f.each_line do |line|
+     words = line.split
+     name = words[2]
+     type = words[1]
+    puts name + ": " + GraphQLType(type)
+  end
+end
+
+
+
